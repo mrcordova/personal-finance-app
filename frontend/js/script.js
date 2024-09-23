b84aa9e3161197120ffc874687cf23566d92c588
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-09-23 16:29:36 -0700

    WIP budgets.html

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,5 +1,37 @@
 // const dataResponse = await fetch("/api/data");
-const URL = "http://localhost:3000";
-const dataResponse = await fetch(`${URL}/api/data`);
-const data = await dataResponse.json();
-console.log(data);
+// const URL = "http://localhost:3000";
+// const dataResponse = await fetch(`${URL}/api/data`);
+// const data = await dataResponse.json();
+// console.log(data);
+// const transactionsBtn = document.querySelector("#transaction-btn");
+// const budgetsBtn = document.querySelector("#budgets-btn");
+const sidebarMenu = document.getElementById("sidebar-menu");
+const main = document.querySelector("main");
+// console.log(main);
+// Initialize the DOM parser
+const parser = new DOMParser();
+
+// Parse the text
+// const doc = parser.parseFromString(html, "text/html");
+const templates = {
+  index: await extractTemplate("index"),
+  transactions: await extractTemplate("transactions"),
+  budgets: await extractTemplate("budgets"),
+  pots: await extractTemplate("pots"),
+  recurring: await extractTemplate("recurring"),
+};
+
+async function extractTemplate(id) {
+  return parser
+    .parseFromString(await (await fetch(`./${id}.html`)).text(), "text/html")
+    .querySelector(`#${id}-template`);
+}
+console.log(templates);
+
+sidebarMenu.addEventListener("click", async (e) => {
+  const liEle = e.target.closest("li");
+  if (liEle) {
+    const clone = templates[`${liEle.dataset.menu}`].content.cloneNode(true);
+    main.replaceChildren(clone);
+  }
+});
